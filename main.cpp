860d4a402f3ca6c7d8473e738d9ffc14dc801a0e
Matthew Wong <wongmat@oregonstate.edu>
2021-01-12 18:31:45 -0800

    new commit with new comparator function and our test CSV

--- main.cpp
+++ main.cpp
@@ -1,3 +1,27 @@
-#include <stdio.h>                    //standard template library
-#include <stdlib.h>                   //
+#include <stdio.h>                    //standard input/output
+#include <stdlib.h>                   //standard library
 #include <string.h>                   //this allows us to use strings in c
+
+//we first need a struct defined to store the movies in for our linked list
+//according to the csv there needs to be a title/ year/ languages/ rating value
+struct movie
+{
+  char title[1000];         //we can store the title as a char
+  float rating;             //we can store the rating as a float
+  int year;                 //we can store the year as an int
+  char lang[1000];          //we can store the language as a char
+};
+
+//we need a sort function to compare and sort out our movies released by year
+//first build out a comparison function to check for the year
+int comparator(const void *a, const void *b)  //we take in two voids as pointers
+{
+  if (((struct movie *)a)-> year == ((struct movie*)b)->year)   //if statement to cheeck if the movie *a pointer this year is equal to b
+    {
+      return ((struct movie)*a)->rating < ((struct movie)*b)->rating; //if it is we return that the ratings
+    }
+  else
+  {
+    return ((struct movie *)a)->year < ((struct movie *)b)-> year;  //otherwise return that the year is less so it compares
+  }
+}

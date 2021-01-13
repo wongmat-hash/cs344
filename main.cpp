85197284f0f866be1de34d6185b4f0e9cdc2cf06
Matthew Wong <wongmat@oregonstate.edu>
2021-01-12 18:19:43 -0800

    first commit with our first cpp file for hw 1

---
+++ main.cpp
@@ -0,0 +1,3 @@
+#include <stdio.h>                    //standard template library
+#include <stdlib.h>                   //
+#include <string.h>                   //this allows us to use strings in c

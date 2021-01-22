1a7abfa7473cb396dba7e8da6bd88bcf23c08cf4
Matthew Wong <wongmat@oregonstate.edu>
2021-01-22 00:18:38 -0800

    redid error validation in menu system so now it works correctly in a loop

--- Assignment2/main.c
+++ Assignment2/main.c
@@ -5,58 +5,69 @@
 #include <string.h>
 
 
+int menuA()                                                                     //first menu prompt
+{
+  int userChoice;
+  printf("1. Select file to process\n");                                        //menu prompt
+  printf("2. Exit the program\n");
+  printf("Enter a choice 1 or 2: ");
+  scanf(" %d", &userChoice);                                                    //takes in user input and stores it in userChoice
+  printf("\n");
+  return userChoice;
+}
+
+int menuB()                                                                     //second menu prompt
+{
+  int userOption;
+  printf("Which file do you want to process?\n");
+  printf("Enter 1 to pick the largest file\n");
+  printf("Enter 2 to pick the smallest file\n");
+  printf("Enter 3 to specify the name of a file\n");
+  printf("Enter a choice from 1 to 3: ");
+  scanf(" %d", &userOption);
+  printf("\n");
+  return userOption;
+}
+
 int main()
 {
-  int userChoice, userOption;                                                   //stores the user choice variable
-  while (1)
-  {
-    printf("1. Select file to process\n");                                      //menu prompt
-    printf("2. Exit the program\n");
-    printf("Enter a choice 1 or 2: ");
-    scanf("%d", &userChoice);                                                   //takes in user input and stores it in userChoice
-    printf("\n");
-    //test to see userChoice was stored correctly
-    //printf("UserChoice: %d\n", userChoice);
-
-    switch (userChoice)                                                         //switch for user based input from menu
+  int userChoice, userOption;
+  do
+  {                                                                             //our do while loop helps our error validation
+    userChoice = menuA();                                                       //set the return value to what the menu prompts for userChoice 1-2
+    if (userChoice == 1)                                                        //if the user enters 1 we need to go to secondary menu
     {
-      case 1:                                                                   //if the user selects a file to process
-      {
-        //need to build out process to take in file
-        printf("Which file do you want to process?\n");
-        printf("Enter 1 to pick the largest file\n");
-        printf("Enter 2 to pick the smallest file\n");
-        printf("Enter 3 to specify the name of a file\n");
-        printf("Enter a choice from 1 to 3: ");
-        scanf("%d", &userOption);
-        printf("\n");
-        //test to see that userOption was stored correctly
-        //printf("UserOption: %d\n", userOption);
-
-        switch(userOption)                                                      //switch for UserOption
+      do
+      {                                                                         //secondary menu which will prompt user 1-3
+        userOption = menuB();
+        if (userChoice == 1)
         {
-          case 1:                                                               //picks the largest file in the directory
-          {
-            //build out largest function
-          }
-          case 2:                                                               //picks the smallest file in the directory
-          {
-            //build out smallest file
-          }
-          case 3:                                                               //specifies a name that the user can open
-          {
-            //build out user specified file
-
-          }
+          //make function for largest file in directory
         }
-      }
-      case 2:                                                                   //exits the program
-      {
-        exit(0);                                                                //exits the program
-      }
+        if (userChoice == 2)
+        {
+          //make a function for smallest file in directory
+        }
+        if (userChoice == 3)
+        {
+          //makes a function for user entry file in directory
+        }
+        else                                                                    //reprompt user which loops
+        {
+          printf("Please enter a valid menu entry\n");
+          //userOption = menuB();                                                 //reprompt so menuB is displayed again
+        }
+      }while (userOption !=1 || userOption !=2 || userOption != 3 || userOption != 4);
     }
-  }
-
-
-  return 0;
+    if (userChoice == 2)
+    {                                                                           //our exit statement
+      exit(0);
+    }
+    else
+    {
+      printf("Please enter a valid menu entry\n");                              //reprompt user so MenuA is displayed again
+      //userChoice = menuA();
+    }
+  }while(userChoice != 1 || userChoice != 2);
+return 0;
 }

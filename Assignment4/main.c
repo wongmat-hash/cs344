17d4f72da79f3f5df703ba1f99765393a65b96c0
Matthew Wong <wongmat@oregonstate.edu>
2021-02-13 16:28:23 -0800

    starting assignment 4 comments all added

--- Assignment4/main.c
+++ Assignment4/main.c
@@ -1,2 +1,14 @@
-//Assignment4
-//Matthew Wong
+//Assignment4 Multi-Threaded Producer Consumer Pipeline
+//Matthew Wong CS344 Winter 2021
+// Write a program that creates 4 threads to process input from standard input as follows:
+//1. Thread 1: called the input thread reads in lines of characters from the standard input
+//2. Thread 2: called the line separator thread replaces every line separator in the input by a space
+//3. Thread 3: callled the plus sign thread replcaes every pair of plus signs, ie "++" by a "^"
+//4. thread 4: called the output thread, write the processed data to standard output as lines of exactly 80 characters
+// The threads must communicate with each other using the producer-consumer approach
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>

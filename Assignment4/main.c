aaedf4ef7c24af369dfd3ad08ee1a4a12b7e1cbe
Matthew Wong <wongmat@oregonstate.edu>
2021-02-16 19:20:57 -0800

    solved the ++ which is now producing ^ correctly with my for loop

--- Assignment4/main.c
+++ Assignment4/main.c
@@ -21,14 +21,14 @@
 //implement single threaded version 1x
 
 //function to process file/ userinput
-void userInput(char *arr)                                             //work cited: https://www.programiz.com/c-programming/c-arrays-functions
+void userInput(char *arr)                                                       //work cited: https://www.programiz.com/c-programming/c-arrays-functions
 {
-
   for (int i = 0; i < MAX_SIZE; i++)                                            //loop through to the size of our array and scanf into our arr work cited: https://www.geeksforgeeks.org/why-to-use-fgets-over-scanf-in-c/
   {
-    scanf("%c", &arr[i]);
+    scanf("%c", &arr[i]);                                                       //use scanf since we know buffer size 1000 and put it into our arr
   }
 
+//OLD STUFF TO TEST//
   //do
   //{
   //  fgets(arr, MAX_SIZE, stdin);
@@ -44,8 +44,7 @@ void userInput(char *arr)                                             //work cit
   //{
   //fgets(arr, MAX_SIZE, stdin);
   //  count--;
-  //}while (count != 0);
-                                                                                //work cited: https://beginnersbook.com/2014/01/c-passing-array-to-function-example/
+  //}while (count != 0);                                                        //work cited: //work cited: https://beginnersbook.com/2014/01/c-passing-array-to-function-example/
   //scanf("%255s", arr);
   printf("this is your string in USERINPUT:\n%s\n", arr);                       //store user input up to 1000 chars in our array
   //now the array has user input or user specified input from < when starting program
@@ -54,29 +53,41 @@ void userInput(char *arr)                                             //work cit
 //function called line seperator thread replcaes every line seperator in the input by a space
 void lineSeperator(char *arr)
 {
-  printf("in line seperator function\n");
-  for (int i = 0; i < MAX_SIZE; i++)
+  printf("in line seperator function\n");                                       //test that we are in the function
+  for (int i = 0; i < MAX_SIZE; i++)                                            //use a for loop to iterate the through the array, find \n and then delete with space
   {
-    if (arr[i] == '\n')                                             //work cited: https://stackoverflow.com/questions/13106108/strcmp-and-new-line-characters-from-text-file
+    if (arr[i] == '\n')                                                         //work cited: https://stackoverflow.com/questions/13106108/strcmp-and-new-line-characters-from-text-file
     {
-      arr[i] = arr[i+1];                                                         //delete that line seperator
+      //arr[i] = arr[i+1];                                                      //delete that line seperator
+      arr[i] = ' ';                                                             //error working with spaces above. Now without quotes it functions correctly work cited: https://stackoverflow.com/questions/30033582/what-is-the-symbol-for-whitespace-in-c
     }
   }
 
-
-
-  //size_t length = strlen(arr);                                                  //now we have hte length of the array
+//OLD WORK//
+  //size_t length = strlen(arr);                                                //now we have hte length of the array
   //printf("\nsize_t: %zu\n", length);
   //if (arr[length-1] == '\n')
   //{
-  //  arr[--length]= '\0';                                                        //work cited: https://stackoverflow.com/questions/28429625/check-if-string-contains-new-line-character
+  //  arr[--length]= '\0';                                                      //work cited: https://stackoverflow.com/questions/28429625/check-if-string-contains-new-line-character
   //}
 }
 //function called plus sign thread replaces every pair of ++ with ^
 void plusplusSign(char *arr)
 {
   printf("in plusplus sign function\n");
-  printf("testing this is MAX_SIZE: %d\n", MAX_SIZE);
+  for (int i = 0; i < MAX_SIZE; i++)
+  {
+    if ((arr[i] == '+') && (arr[i+1] == '+'))
+    {
+      for (int x = i; x < MAX_SIZE; x++)
+      {
+        arr[x] = arr[x+1];
+      }
+      arr[i] = '^';
+    }
+  }
+  //OLD STUFF//
+  //printf("testing this is MAX_SIZE: %d\n", MAX_SIZE);
   //for (int i = 0; i < MAX_SIZE; i++)
   //{
   //  printf("%s ", &arr[i]);
@@ -88,7 +99,7 @@ void plusplusSign(char *arr)
 int main(int argc, char* argv[])
 {
   char ar[MAX_SIZE];                                                            //char array of 1000 that will store user input or standard argument
-  userInput(ar);                                                                 //pass the array into user Input to grab and store data
+  userInput(ar);                                                                //pass the array into user Input to grab and store data
 
   //check that the user input is stored correctly TEST
   //printf("this is your string in MAIN:\n%s\n", ar);                             //TESTS to make sure our values are passing around correctly
@@ -106,6 +117,7 @@ int main(int argc, char* argv[])
   printf("\nTEST\n");
   printf("\n%d\n", x);
 
+  //now pass into our plusplusSign function which will remove ++ and replace with ^
   plusplusSign(ar);
   printf("\n\nnow back in main after plusplusSign\n");
   printf("AR array[0] test: %c\n", ar[0]);

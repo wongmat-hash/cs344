668780c64bd7bbe6340d81a5fcb0e427de71824a
Matthew Wong <wongmat@oregonstate.edu>
2021-03-12 22:06:57 -0800

    commented in all the comments

--- Assignment6/main.rs
+++ Assignment6/main.rs
@@ -9,9 +9,11 @@ use std::env; // to get arugments passed to the program
 * Print the number of partitions and the size of each partition
 * @param vs A vector of vectors
 */
-fn print_partition_info(vs: &Vec<Vec<usize>>){
+fn print_partition_info(vs: &Vec<Vec<usize>>)
+{
     println!("Number of partitions = {}", vs.len());
-    for i in 0..vs.len(){
+    for i in 0..vs.len()
+    {
         println!("\tsize of partition {} = {}", i, vs[i].len());
     }
 }
@@ -21,9 +23,11 @@ fn print_partition_info(vs: &Vec<Vec<usize>>){
 * @param num_elements How many integers to generate
 * @return A vector with integers from 0 to (num_elements - 1)
 */
-fn generate_data(num_elements: usize) -> Vec<usize>{
+fn generate_data(num_elements: usize) -> Vec<usize>
+{
     let mut v : Vec<usize> = Vec::new();
-    for i in 0..num_elements {
+    for i in 0..num_elements
+    {
         v.push(i);
     }
     return v;
@@ -35,7 +39,8 @@ fn generate_data(num_elements: usize) -> Vec<usize>{
 * @return A vector that contains 2 vectors of integers
 
 */
-fn partition_data_in_two(v: &Vec<usize>) -> Vec<Vec<usize>>{
+fn partition_data_in_two(v: &Vec<usize>) -> Vec<Vec<usize>>
+{
     let partition_size = v.len() / 2;
     // Create a vector that will contain vectors of integers
     let mut xs: Vec<Vec<usize>> = Vec::new();
@@ -43,7 +48,8 @@ fn partition_data_in_two(v: &Vec<usize>) -> Vec<Vec<usize>>{
     // Create the first vector of integers
     let mut x1 : Vec<usize> = Vec::new();
     // Add the first half of the integers in the input vector to x1
-    for i in 0..partition_size{
+    for i in 0..partition_size
+    {
         x1.push(v[i]);
     }
     // Add x1 to the vector that will be returned by this function
@@ -52,7 +58,8 @@ fn partition_data_in_two(v: &Vec<usize>) -> Vec<Vec<usize>>{
     // Create the second vector of integers
     let mut x2 : Vec<usize> = Vec::new();
     // Add the second half of the integers in the input vector to x2
-    for i in partition_size..v.len(){
+    for i in partition_size..v.len()
+    {
         x2.push(v[i]);
     }
     // Add x2 to the vector that will be returned by this function
@@ -68,9 +75,11 @@ fn partition_data_in_two(v: &Vec<usize>) -> Vec<Vec<usize>>{
 * Note: this function has the same code as the reduce_data function.
 *       But don't change the code of map_data or reduce_data.
 */
-fn map_data(v: &Vec<usize>) -> usize{
+fn map_data(v: &Vec<usize>) -> usize
+{
     let mut sum = 0;
-    for i in v{
+    for i in v
+    {
         sum += i;
     }
     sum
@@ -81,9 +90,11 @@ fn map_data(v: &Vec<usize>) -> usize{
 * @param v Vector of integers
 * @return Sum of integers in v
 */
-fn reduce_data(v: &Vec<usize>) -> usize{
+fn reduce_data(v: &Vec<usize>) -> usize
+{
     let mut sum = 0;
-    for i in v{
+    for i in v
+    {
         sum += i;
     }
     sum
@@ -92,28 +103,35 @@ fn reduce_data(v: &Vec<usize>) -> usize{
 /*
 * A single threaded map-reduce program
 */
-fn main() {
+//generates data for the rest of the program
+fn main()
+{
 
     // Use std::env to get arguments passed to the program
     let args: Vec<String> = env::args().collect();
-    if args.len() != 3 {
+    if args.len() != 3
+    {
         println!("ERROR: Usage {} num_partitions num_elements", args[0]);
         return;
     }
     let num_partitions : usize = args[1].parse().unwrap();
     let num_elements : usize = args[2].parse().unwrap();
-    if num_partitions < 1{
+    if num_partitions < 1
+    {
       println!("ERROR: num_partitions must be at least 1");
         return;
     }
-    if num_elements < num_partitions{
+    if num_elements < num_partitions
+    {
         println!("ERROR: num_elements cannot be smaller than num_partitions");
         return;
     }
 
+    //calls generate data to generate a vector of numbers that serves as input for the rest of the program
     // Generate data.
     let v = generate_data(num_elements);
 
+    //calls partition which iwll partition the input numbers into two parititons
     // PARTITION STEP: partition the data into 2 partitions
     let xs = partition_data_in_two(&v);
 
@@ -127,7 +145,7 @@ fn main() {
     // CHANGE CODE START: Don't change any code above this line
 
     // Change the following code to create 2 threads that run concurrently and each of which uses map_data() function to process one of the two partitions
-
+    //returns sum of all the numbers in that partition
     intermediate_sums.push(map_data(&xs[0]));
     intermediate_sums.push(map_data(&xs[1]));
 
@@ -136,6 +154,7 @@ fn main() {
     // Print the vector with the intermediate sums
     println!("Intermediate sums = {:?}", intermediate_sums);
 
+    //sums the intermediate results produced by the map step to produce the final step of all the input numbers ß
     // REDUCE STEP: Process the intermediate result to produce the final result
     let sum = reduce_data(&intermediate_sums);
     println!("Sum = {}", sum);
